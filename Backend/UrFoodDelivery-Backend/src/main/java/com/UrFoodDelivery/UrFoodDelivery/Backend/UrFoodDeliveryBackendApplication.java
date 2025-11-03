2c457a99123430a12f52469110be9e826aa96eeb
Devershi Dalwadi <devershi30@gmail.com>
2025-11-03 13:04:31 -0600

    To Initialize Springboot Backend with Maven build configuration.

---
+++ Backend/UrFoodDelivery-Backend/src/main/java/com/UrFoodDelivery/UrFoodDelivery/Backend/UrFoodDeliveryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.UrFoodDelivery.UrFoodDelivery.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrFoodDeliveryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrFoodDeliveryBackendApplication.class, args);
+	}
+
+}
