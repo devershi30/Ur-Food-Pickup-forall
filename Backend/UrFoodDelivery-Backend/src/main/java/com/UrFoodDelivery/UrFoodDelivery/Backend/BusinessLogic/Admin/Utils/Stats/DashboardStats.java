d8911b1082ee0905d121087a1bf9b4ea2f93e0cd
apx848 <aksharjpatel2002@gmail.com>
2025-11-12 20:21:26 -0600

    Add files via upload

---
+++ Backend/UrFoodDelivery-Backend/src/main/java/com/UrFoodDelivery/UrFoodDelivery/Backend/BusinessLogic/Admin/Utils/Stats/DashboardStats.java
@@ -0,0 +1,15 @@
+package com.UrFoodDelivery.UrFoodDelivery.Backend.BusinessLogic.Admin.Utils.stats;
+
+
+import lombok.Data;
+
+@Data
+public class DashboardStats {
+
+    private int totalUsers;
+
+    private int totalOrders;
+
+    private int totalVendors;
+
+}
