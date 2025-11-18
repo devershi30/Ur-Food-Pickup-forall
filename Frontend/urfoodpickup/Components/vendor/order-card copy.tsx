87ca28f2b2e2599d1e6466ead689d665633522c6
Ishan Patel <176358833+ipp628@users.noreply.github.com>
2025-11-18 01:28:25 -0600

    Update order-card copy.tsx
    error fixed

--- Frontend/urfoodpickup/Components/vendor/order-card copy.tsx
+++ Frontend/urfoodpickup/Components/vendor/order-card copy.tsx
@@ -5,7 +5,7 @@ import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Clock, User, MapPin, Package } from "lucide-react" 
 import type { VendorOrder } from "@/lib/vendor-mock-data"
-
+ 
 interface OrderCardProps {
   order: VendorOrder
   onStatusChange: (orderId: string, newStatus: VendorOrder["status"]) => void
